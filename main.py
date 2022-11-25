74fceb1ee7f87b3412626e2079160e8de4743e66
williamhCode <83525937+williamhCode@users.noreply.github.com>
2022-11-25 08:12:29 -0500

    update pyglet example

--- main.py
+++ main.py
@@ -6,22 +6,26 @@ from OpenGL.GL import *
 
 from engine.timer import Timer
 import engine
+from engine.render import Renderer
 
 class MainWindow(pyglet.window.Window):
     
     def __init__(self):
         config = pyglet.gl.Config(major_version=4, minor_version=1, forward_compatible=True, double_buffer=True, depth_size=24)
-        super().__init__(width=1280, height=720, vsync=False, resizable=True, config=config)
+        super().__init__(width=1200, height=800, vsync=False, resizable=True, config=config)
 
-        engine.render.init()
+        self.renderer = Renderer()
+        self.renderer.set_clear_color((50, 50, 50, 255))
+
+        self.renderer.set_size(1200, 800)
         
-        self.test1_tex_id = engine.texture.Texture.from_path('imgs/test.png')
-        self.test2_tex_id = engine.texture.Texture.from_path('imgs/test2.jpeg')
+        # self.test1_tex_id = engine.texture.Texture.from_path('imgs/test.png')
+        # self.test2_tex_id = engine.texture.Texture.from_path('imgs/test2.jpeg')
 
         self.timer = Timer()
         
-        # self.event_loop()
-        pyglet.clock.schedule_interval(self.update, 1.0/60.0)
+        self.event_loop()
+        # pyglet.clock.schedule_interval(self.update, 1.0/60.0)
         
     def on_key_press(self, symbol, modifiers):
         super().on_key_press(symbol, modifiers)
@@ -30,46 +34,33 @@ class MainWindow(pyglet.window.Window):
         pixel_width, pixel_height = self.get_framebuffer_size()
         glViewport(0, 0, pixel_width, pixel_height)
 
-        engine.render.on_resize(width, height)
-
     def on_draw(self):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        engine.render.begin()
 
-        for i in range(100):
-            for j in range(100):
-                engine.render.draw_colored_quad((255, 0, 0, 255), (i * 5, j * 5), (4, 4))
+        self.renderer.begin()
+        self.renderer.clear()
 
-        # for i in range(10):
-        #     for j in range(10):
-        #         engine.render.draw_textured_quad(self.test1_tex_id, (250 + i * 50, 250 + j * 50), (49, 49))
+        for i in range(200):
+            for j in range(200):
+                self.renderer.draw_rectangle((200, 0, 0, 255), (i * 10, j * 10), (8, 8), 10, width=1, fade=1)
 
-        # for i in range(100):
-        #     for j in range(100):
-        #         engine.render.draw_circle((255, 255, 0, 255), (i * 10, j * 10), 5, width=1, fade=0.2)
+        self.renderer.end()
 
-        engine.render.draw_circle((255, 0, 0, 255), (500, 300), 100, width=20, fade=0.1)
-        engine.render.draw_circle((0, 255, 255, 240), (600, 300), 50, width=0, fade=0.02)
-        # for i in range(10000):
-        #     engine.render.draw_rectangle((255, 0, 0, 255), (500, 300), (100, 100), rotation=20)
-
-        engine.render.end()
-
-        # self.flip()
+        self.flip()
     
-    def update(self, dt):
-        dt = self.timer.tick()
-        self.set_caption(f'{self.timer.get_fps():.2f} FPS')
+    # def update(self, dt):
+    #     dt = self.timer.tick()
+    #     self.set_caption(f'{self.timer.get_fps():.2f} FPS')
                 
-    # def event_loop(self):
-        
-    #     while not self.has_exit:
-    #         self.dispatch_events()
+    def event_loop(self):
+        while not self.has_exit:
+            self.dispatch_events()
             
-    #         dt = self.timer.tick()
-    #         self.set_caption(f'{self.timer.get_fps():.2f} FPS')
+            dt = self.timer.tick()
+            self.set_caption(f'{self.timer.get_fps():.2f} FPS')
             
-    #         self.dispatch_event('on_draw')
+            self.dispatch_event('on_draw')
+
         
 if __name__ == '__main__':
     window = MainWindow()
