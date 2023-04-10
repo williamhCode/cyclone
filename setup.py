48985f8776b446a8984154f5f272f7e0343278a3
williamhCode <83525937+williamhCode@users.noreply.github.com>
2023-04-10 05:06:35 -0400

    cmake integration + fix build

--- setup.py
+++ setup.py
@@ -1,9 +1,52 @@
 import os
-from os import path
+import sys
+import subprocess
 from glob import glob
+import pathlib
 
 from Cython.Build import cythonize
 from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext as _build_ext
+
+
+# options
+release = True
+build_libs = False
+
+annotate = False
+force = False
+quiet = False
+
+
+# build_ext from cmake
+# cmake -E make_directory cmake_build
+# cmake -E chdir cmake_build cmake ..
+# cmake --build cmake_build
+class build_ext(_build_ext):
+    def run(self):
+        self.build_cmake()
+        super().run()
+
+    def build_cmake(self):
+        cwd = pathlib.Path().absolute()
+
+        env = os.environ.copy()
+        build_temp = pathlib.Path(self.build_temp)
+        build_temp.mkdir(parents=True, exist_ok=True)
+
+        subprocess.check_call(
+            ["cmake", "-E", "make_directory", self.build_temp],
+            env=env,
+        )
+        subprocess.check_call(
+            ["cmake", "-E", "chdir", self.build_temp, "cmake", cwd],
+            env=env,
+        )
+        subprocess.check_call(
+            ["cmake", "--build", self.build_temp, "--config", "Release"],
+            env=env,
+        )
+
 
 os.environ["CC"] = "gcc"
 
@@ -12,7 +55,7 @@ files = []
 for dirpath, _, filenames in os.walk("cyclone"):
     for filename in filenames:
         if filename.endswith("pyx"):
-            ext_path = path.join(dirpath, filename)
+            ext_path = os.path.join(dirpath, filename)
             ext_name = ext_path.replace("/", ".").replace(".pyx", "")
             files.append((ext_name, ext_path))
 
@@ -29,6 +72,11 @@ library_dirs = [
     "lib",
 ]
 
+runtime_library_dirs = [
+    os.getcwd() + "/lib",  # for editable install
+    sys.prefix + "/lib",  # for non-editable install
+]
+
 libraries = [
     "glfw",
     "glad",
@@ -43,15 +91,12 @@ macros = [
 
 # compiler -------------------------------------
 language = "c"
-default_args = ["-std=c17", "-w"]
-debug_args = ["-std=c17", "-w", "-O0"]
-release_args = ["-std=c17", "-w", "-O3", "-ffast-math", "-march=native"]
-args = debug_args
-
-# cythonize options
-annotate = True
-force = False
-quiet = False
+debug_args = ["-w", "-O0"]
+release_args = ["-w", "-O3"]
+if release:
+    args = release_args
+else:
+    args = debug_args
 
 # setup ----------------------------------------
 extensions = []
@@ -63,7 +108,7 @@ for ext_name, ext_path in files:
         library_dirs=library_dirs,
         libraries=libraries,
         define_macros=macros,
-        runtime_library_dirs=library_dirs,
+        runtime_library_dirs=runtime_library_dirs,
         language=language,
         extra_compile_args=args,
     )
@@ -89,6 +134,7 @@ setup(
         force=force,
         quiet=quiet,
     ),
+    cmdclass={"build_ext": build_ext if build_libs else _build_ext},
     packages=packages,
     package_data=package_data,
     data_files=data_files,
